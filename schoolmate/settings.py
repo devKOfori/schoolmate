24ad10fdcd0cd1176970abcadaf3a163f046fa72
inconsistentDev <groupfivethree@gmail.com>
2024-01-15 04:09:59 +0200

    Add hostel app to installed apps

--- schoolmate/settings.py
+++ schoolmate/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # added apps
+    'hostel.apps.HostelConfig',
 ]
 
 MIDDLEWARE = [
