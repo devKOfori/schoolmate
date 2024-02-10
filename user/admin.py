d89ffa34ae3d081cdb3f7a6e06ea1d08ab9a4063
devKOfori <groupfivethree@gmail.com>
2024-02-10 11:22:04 +0200

    Update user models and admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+from user.models import CustomUser
+
+admin.site.register(CustomUser)
