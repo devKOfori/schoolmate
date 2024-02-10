195bf084bfecda4f22a1929ddc4afa62120a39c8
devKOfori <groupfivethree@gmail.com>
2024-02-10 11:21:07 +0200

    Update school models and admin

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import (
+    Gender, MaritalStatus, Relation, Nationality
+)
 # Register your models here.
+admin.site.register(Gender)
+admin.site.register(MaritalStatus)
+admin.site.register(Relation)
+admin.site.register(Nationality)
--- school/models.py
+++ school/models.py
@@ -25,3 +25,4 @@ class Relation(models.Model):
 
     def __str__(self):
         return self.name
+    
