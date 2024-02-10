dd2826c8837906fa97de929a3780aaaa613ab1eb
devKOfori <groupfivethree@gmail.com>
2024-02-10 02:28:24 +0200

    Update school model

--- school/models.py
+++ school/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Gender(models.Model):
+    name = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.name
+
+class Nationality(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class MaritalStatus(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Relation(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
