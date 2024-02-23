c6134baa57207caf529112d46698baa9c6119644
devKOfori <groupfivethree@gmail.com>
2024-02-23 13:55:55 +0200

    Solve unhashed password error

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+@admin.register(CustomUser)
+class UserAdmin(UserAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'is_staff')
+    search_fields = ('email',)
+    ordering = ('email',)
