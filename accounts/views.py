43f32bdb66b0966c5e14bdc119bc23fe6ce0a15c
devKOfori <groupfivethree@gmail.com>
2024-06-30 23:18:37 +0300

    Update accounts views and urls

--- accounts/views.py
+++ accounts/views.py
@@ -7,13 +7,15 @@ def login(request):
     if request.method == "POST":
         email = request.POST.get("email", None)
         password = request.POST.get("password", None)
+        print(email, password)
         if all([email, password]):
             user = authenticate(request, username=email, password=password)
             if user is not None:
                 auth_login(request, user)
-                return redirect("homepage")
+                return redirect("dashboard")
             else:
                 messages.error(request, "Invalid Credentials")
+                return render(request, "account/login.html")
     else:
         return render(request, "account/login.html")
     
@@ -24,14 +26,14 @@ def register(request):
         password2 = request.POST.get("password2")
         if all([email, password, password2]) and password2==password:
             usermodel = get_user_model()
-            _ = usermodel.objects.create_user(email=email, password=password)
-            return redirect("homepage")
+            _ = usermodel.objects.create_user(email, password)
+            return redirect("login")
         else:
             messages.error(request, "Correct the errors and try again")
             return render(request, "account/register.html")
     else:
         return render(request, "account/register.html")
     
-def logout(request):
+def logout_view(request):
     logout(request)
     return redirect("login")
