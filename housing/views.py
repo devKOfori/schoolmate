3ca0427bd369649af0c629298b8caba5efaadf98
devKOfori <groupfivethree@gmail.com>
2024-07-03 21:08:11 +0300

    Update hostel creation form

--- housing/views.py
+++ housing/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from . import models as housing_models
 from . import forms as housing_forms
+from hms import models as hms_models
 
 class CreateHostelView(generic.CreateView):
     model = housing_models.Hostels
@@ -11,7 +12,12 @@ class CreateHostelView(generic.CreateView):
     
     def get_success_url(self):
         return reverse_lazy("configure-hostel", kwargs={"nameslug": self.object.nameslug})
-
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cities = hms_models.City.objects.all()
+        context["cities"] = cities
+        return context
 
 def configure_hostel(request, nameslug):
     hostel = housing_models.Hostels.objects.get(nameslug=nameslug)
